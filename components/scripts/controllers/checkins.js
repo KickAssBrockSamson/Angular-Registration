350dc7b00d76805d7d47535cb41dc3572656c8be
Travis Mikolay <travisemikolay@gmail.com>
2015-12-20 16:29:15 -0500

    Added Checkins JavaScript

--- components/scripts/controllers/checkins.js
+++ components/scripts/controllers/checkins.js
@@ -2,4 +2,13 @@ myApp.controller('CheckInsController',
   ['$scope', '$rootScope', '$firebaseObject', '$firebaseArray','$routeParams', 'FIREBASE_URL',
   function($scope, $rootScope, $firebaseObject, $firebaseArray, $routeParams, FIREBASE_URL) {
 
+    $scope.whichmeeting = $routeParams.mId;
+    $scope.whichuser = $routeParams.uId;
+
+    var ref = new firebase(FIREBASE_URL + 'users/' + $scope.whichuser + '/meetings/' + $scope.whichmeeting + '/checkins'); 
+
+    $scope.addCheckin = function() {
+      var checkinsInfo = $firebaseArray(ref)
+    }
+
 }]); //Controller    
