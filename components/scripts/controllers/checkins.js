fdbadd73243db898c35ce7d460b640b4a9d0042f
Travis Mikolay <travisemikolay@gmail.com>
2015-12-20 19:27:37 -0500

    Add / Remove Class function

--- components/scripts/controllers/checkins.js
+++ components/scripts/controllers/checkins.js
@@ -12,20 +12,61 @@ myApp.controller('CheckInsController',
     var checkinsList = $firebaseArray(ref);
     $scope.checkins = checkinsList;
 
+    $scope.order = "firstname";
+    $scope.direction = null;
+    $scope.query = '';
+    $scope.recordId='';
+
+
     $scope.addCheckin = function() {
-     var checkinsInfo = $firebaseArray(ref);
-     var myData = {
-      firstname: $scope.user.firstname,
-      lastname: $scope.user.lastname,
-      email: $scope.user.email,
-      date: Firebase.ServerValue.TIMESTAMP
-     }; //myData
+      var checkinsInfo = $firebaseArray(ref);
+      var myData = {
+        firstname: $scope.user.firstname,
+        lastname: $scope.user.lastname,
+        email: $scope.user.email,
+        date: Firebase.ServerValue.TIMESTAMP
+      }; //myData
 
-     checkinsInfo.$add(myData).then(function() {
-      $location.path('/checkins/' + $scope.whichuser + '/' +
+      checkinsInfo.$add(myData).then(function() {
+        $location.path('/checkins/' + $scope.whichuser + '/' +
         $scope.whichmeeting + '/checkinsList');
-     }); //Send data to Firebase
-
+      }); //Send data to Firebase
     }; //AddCheckin
 
-}]); //Controller 
+    $scope.deleteCheckin = function(id) {
+      var refDel = new Firebase(FIREBASE_URL + 'users/' +
+        $scope.whichuser + '/meetings/' +
+        $scope.whichmeeting + '/checkins/' + id);
+      var record = $firebaseObject(refDel);
+      record.$remove(id);
+    };
+
+    $scope.pickRandom = function() {
+      var whichRecord = Math.round(Math.random()* (checkinsList.length - 1));
+      $scope.recordId = checkinsList.$keyAt(whichRecord);
+    } //pick winner
+
+    $scope.showLove = function(myCheckin) {
+      myCheckin.show = !myCheckin.show;
+
+      if (myCheckin.userState == 'expanded') {
+        myCheckin.userState = ''; 
+      } else {
+        myCheckin.userState = 'expanded';
+      }
+    } // Show Love 
+
+
+
+
+
+}]); //Controller    
+
+
+
+
+
+
+
+
+
